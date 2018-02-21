6321bdc4aa4a3f12c38b31254aab0b31c9bb5430
Arjun Singh <arjunsingh@Arjuns-MacBook-Air.local>
2018-02-21 16:06:39 +0530

    i changed the data types of latitude,longitude and accuracy to varchar because there are empty string in csv files

--- create.sql
+++ create.sql
@@ -2,7 +2,7 @@ CREATE TABLE GEO_DATA(
     id VARCHAR PRIMARY KEY,
     place_name VARCHAR NOT NULL,
     admin_name VARCHAR NOT NULL,
-    latitude DOUBLE PRECISION ,
-    longitude DOUBLE PRECISION ,
-    accuracy INTEGER 
+    latitude VARCHAR ,
+    longitude VARCHAR ,
+    accuracy VARCHAR
 );
