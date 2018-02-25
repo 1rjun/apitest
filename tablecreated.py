29bc3880b1b4df5d897503689359c731640db4ba
Arjun Singh <arjunsingh@Arjuns-MacBook-Air.local>
2018-02-25 22:32:21 +0530

    define about table

--- tablecreated.py
+++ tablecreated.py
@@ -5,9 +5,11 @@ This is a python script to put the csv data in our postgresql table GEO_DAT
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
 from csv import reader as read
-#here i database engine which connect with the database ,and 1st arjunsingh is my username and second arjunsingh is my database username
-#and mydb is my postgresql database name which is available in my local computer
-#you can put yours link in create_engine whenever you try to run this script
+# here i database engine which connect with the database ,and 1st arjunsingh is my username and second arjunsingh is my database username
+# and mydb is my postgresql database name which is available in my local computer
+# you can put yours link in create_engine whenever you try to run this script
+
+# make sure to give your postgres database link in create_engine 
 engine = create_engine("postgresql://arjunsingh:arjunsingh@localhost/mydb")
 
 db = scoped_session(sessionmaker(bind=engine))
@@ -16,6 +18,9 @@ def main():
     IN = open("IN.csv","r")
     IN_read = read(IN)
     for id, place_name, admin_name, latitude, longitude, accuracy in IN_read:
+
+        # geo_data is my table name 
+        
         db.execute("INSERT INTO geo_data VALUES (:id,:place_name,:admin_name,:latitude,:longitude,:accuracy)",
         {"id":id,"place_name":place_name,"admin_name":admin_name,"latitude":latitude,"longitude":longitude,
         "accuracy":accuracy})
